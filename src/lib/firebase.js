1181583866684f33ef4b673e9d4b20cc668a0bdd
ZeckRoom <83294681+ZeckRoom@users.noreply.github.com>
2026-01-14 02:38:01 +0100

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 // TODO: Reemplaza con tus credenciales de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBDh0wZy52RrOrQ0XOkAa2BFa7y7bLGARQ",
+  authDomain: "beverage-warehouse-mvp.firebaseapp.com",
+  projectId: "beverage-warehouse-mvp",
+  storageBucket: "beverage-warehouse-mvp.firebasestorage.app",
+  messagingSenderId: "700953591013",
+  appId: "1:700953591013:web:96e1ef2a4e97977e55680c"
 };
 
 // Inicializar Firebase
